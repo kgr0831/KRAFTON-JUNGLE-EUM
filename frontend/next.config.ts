3ebc8e787cbf0f73765935a242a9af20e6c91160
JUNGBOGEON <pyeonno13634@gmail.com>
2025-12-30 17:25:09 +0900

    amplify 동적 라우팅

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,38 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-    ],
-  },
-  async headers() {
-    return [
-      {
-        source: "/:path*.wasm",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/wasm",
-          },
-        ],
-      },
-      {
-        source: "/:path*.onnx",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/octet-stream",
-          },
-        ],
-      },
-    ];
+    unoptimized: true,
   },
 };
 
